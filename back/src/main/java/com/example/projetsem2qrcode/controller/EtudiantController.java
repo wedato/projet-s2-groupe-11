0db54ca234a47a7cd904f5e85b97dd313f622771
JonathanBaltaci <genzio775@gmail.com>
2022-03-29 16:38:35 +0200

    add crossorigin

--- back/src/main/java/com/example/projetsem2qrcode/controller/EtudiantController.java
+++ back/src/main/java/com/example/projetsem2qrcode/controller/EtudiantController.java
@@ -5,6 +5,7 @@ import com.example.projetsem2qrcode.modele.Etudiant;
 import com.example.projetsem2qrcode.service.EtudiantService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,8 @@ public class EtudiantController {
     @Autowired
     private final EtudiantService etudiantService;
 
-    @GetMapping("/products")
+    @GetMapping("/etudiants")
+    @CrossOrigin(origins = "http://localhost:4200")
     public List<Etudiant> fetchAllEtudiants() {
         return etudiantService.getAllEtudiants();
     }
