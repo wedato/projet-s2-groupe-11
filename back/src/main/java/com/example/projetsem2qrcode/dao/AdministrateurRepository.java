e9f70cef3c8e9714b1f278a1875cd17f7fb72a42
o2163453 <clement.guyot@etu.univ-orleans.fr>
2022-03-29 17:24:15 +0200

    contruction de l'entity Administrateur

--- back/src/main/java/com/example/projetsem2qrcode/dao/AdministrateurRepository.java
+++ back/src/main/java/com/example/projetsem2qrcode/dao/AdministrateurRepository.java
@@ -1,11 +1,10 @@
 package com.example.projetsem2qrcode.dao;
 
-import com.example.projetsem2qrcode.modele.Etudiant;
+import com.example.projetsem2qrcode.modele.Administrateur;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface EtudiantRepository extends MongoRepository<Etudiant,String> {
+public interface AdministrateurRepository extends MongoRepository<Administrateur, String> {
+
 }
---
+++ back/src/main/java/com/example/projetsem2qrcode/exception/EtudiantInexistantException.java
@@ -0,0 +1,7 @@
+package com.example.projetsem2qrcode.exception;
+
+public class EtudiantInexistantException extends Exception {
+    public EtudiantInexistantException(){
+        super("Etudiant inexistant");
+    }
+}
